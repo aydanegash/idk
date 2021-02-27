c8c36e5ade2ffd71958666e02c84810431971269
Ayda Negash <aydanegash@gmail.com>
2021-02-26 21:46:22 -0600

    Css and Js update

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 
-console.log("testing");
-console.log("this");
+//alert("Would you like to create a random computer generated password?");
 
 
+//Array of different charachters that can be used in password
+var capitalLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
+                        "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
+var lowerCaseletters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n",
+                        "o","p","q","r","s","t","u","v","w","x","y","z"];
 
+var specailCharachters = ["`","~","!","@","#","$","%","^","&","*","(",")","-","_",
+                            "=","+",",",".","<",">","/","?",";",":"];
+
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 
