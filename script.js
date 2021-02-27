0837d526fbfb1e0d6e27bbea4e2dd74340a9d718
Ayda Negash <aydanegash@gmail.com>
2021-02-26 22:16:07 -0600

    Added all var to js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 
-//alert("Would you like to create a random computer generated password?");
+alert("Would you like to create a random computer generated password?");
 
+var numberOfcharachters = prompt("Between 8 to 16 charachters, how many charachters would you like your password to have?");
+
+var useOfcapitalLetters = confirm("Would you like to include capital letters?");
+
+var useOflowerCaseLetters = confirm("Would you like to include lower case letters?");
+
+var useOfSpecialCharachters = confirm("Would you like to use special charachters?");
+
+var useOfNumbers = confirm("Would you like to use numbers?");
 
 //Array of different charachters that can be used in password
 var capitalLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
@@ -14,3 +23,4 @@ var specailCharachters = ["`","~","!","@","#","$","%","^","&","*","(",")","-","_
 
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 
+ 
