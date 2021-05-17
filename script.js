42960a5428ffc0cc9c6899cca158da3cfc88824f
aydanegash <73369360+aydanegash@users.noreply.github.com>
2021-05-17 17:11:35 -0500

    console logs for confirm options and begining of math.random function

--- script.js
+++ script.js
@@ -1,39 +1,6 @@
 
  //welcome message
- alert("Would you like to create a random computer generated password?");
-
-
-//var of the prompts/confirm so user can pick what characteristics their password has
-var numberOfcharachters = prompt("Between 8 to 128 characters, how many charachters would you like your password to have?");
-if(numberOfcharachters <8 || numberOfcharachters >128 ) {
-    prompt("Password length must be between 8 and 128 charachters. How Many characters would you like your password to have?");
-    
-}
-
-
-var useOfcapitalLetters = confirm("Click OK to include capital letters.");
-
-var useOflowerCaseLetters = confirm("Click OK to include lower case letters.");
-
-var useOfSpecialCharachters = confirm("Click OK to use special charachters.");
-
-var useOfNumbers = confirm("Click   OK to use numbers.");
-
-
-// //Random index for computer choices for the charachters
-// var index = Math.floor(Math.random() * capitalLetters.length);
-// capitalLetters[index];
-
-// var index = Math.floor(Math.random() * lowerCaseletters.length);
-// lowerCaseletters[index];
-
-// var index = Math.floor(Math.random() * specialCharachters.length);
-// specialCharachters[index];
-
-// var index = Math.floor(Math.random() * numbers.length);
-// numbers[index];
-
-
+ var choiceCreatePassword = alert("Would you like to create a random computer generated password?");
 
 //Array of different charachters that can be used in password
 var capitalLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
@@ -48,7 +15,70 @@ var specialCharachters = ["`","~","!","@","#","$","%","^","&","*","(",")","-","_
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 
 
-//If user chooses password length beyond acccepted range
+// functin to create password 
+var createNewPassword = function() {
+    //var of the prompts/confirm so user can pick what characteristics their password has
+var numberOfcharachters = prompt("Between 8 to 128 characters, how many charachters would you like your password to have?");
+if(numberOfcharachters <8 || numberOfcharachters >128 ) {
+    prompt("Password length must be between 8 and 128 charachters. How Many characters would you like your password to have?");   
+} else {
+    console.log("User password will be " + numberOfcharachters + " characters long.");  
+};
+
+
+// confirm for use of capital letters; console log user choice.
+var useOfcapitalLetters = confirm("Click OK to include capital letters.");
+if (useOfcapitalLetters === true){
+    console.log("Capital letters will be included.")
+} else {
+    console.log("Capital letters will NOT be included.")
+};
+
+//confirm for use of lower case letters; console log user choice.
+var useOflowerCaseLetters = confirm("Click OK to include lower case letters.");
+if (useOflowerCaseLetters === true){
+    console.log("Lowercase letters will be included.") 
+} else {
+    console.log("Lowercase letters will NOT be included.")
+};
+
+//confirm for use of special charachters; console log user choice.
+var useOfSpecialCharachters = confirm("Click OK to use special charachters.");
+if (useOfSpecialCharachters=== true){
+    console.log("Special characters will be included.")
+} else {
+    console.log("Special characthers will NOT be included.")
+};
+
+//confim for use of number; console log user choice.
+var useOfNumbers = confirm("Click   OK to use numbers.");
+if (useOfNumbers === true){
+    console.log("Numbers will be included.")
+} else {
+    console.log("Numbers will NOT be included.")
+};
+
+//Random index for computer choices for the charachters
+var index = (Math.floor(Math.random() * capitalLetters.length));  
+capitalLetters[index];
+
+var index = (Math.floor(Math.random() * lowerCaseletters.length));
+lowerCaseletters[index];
+
+var index = (Math.floor(Math.random() * specialCharachters.length));
+specialCharachters[index];
+
+var index = (Math.floor(Math.random() * numbers.length));
+numbers[index];
+
+
+
+
+
+
+
+
+
 
 
 //If user chooses to use capital letters
@@ -62,4 +92,15 @@ var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 
 //if user chooses to use numbers
 
- 
+
+var createNewPasswordAgain = window.confirm("Would you like to create a new computer generated password again?");
+
+// If user pressed OK, run the function again
+if (createNewPasswordAgain) {
+  createNewPassword();
+    }
+};
+
+
+ //start over
+ createNewPassword();
