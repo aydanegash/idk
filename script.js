f598334ae0bfc625fd2986a7d6a527ac8b13a980
aydanegash <73369360+aydanegash@users.noreply.github.com>
2021-05-19 16:31:33 -0500

    toyed with randomizing the password based on length selected and having it appear in textbox

--- script.js
+++ script.js
@@ -1,79 +1,96 @@
 
- //welcome message
-//var password= "";
-//Array of different charachters that can be used in password
-var capitalLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
-                        "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+//Array of different characters that can be used in password
+    var capitalLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
+                            "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-var lowerCaseletters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n",
-                        "o","p","q","r","s","t","u","v","w","x","y","z"];
+    var lowerCaseletters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n",
+                            "o","p","q","r","s","t","u","v","w","x","y","z"];
 
-var specialCharacters = ["`","~","!","@","#","$","%","^","&","*","(",")","-","_",
-                            "=","+",",",".","<",">","/","?",";",":"];
+    var specialCharacters = ["`","~","!","@","#","$","%","^","&","*","(",")","-","_",
+                                "=","+",",",".","<",">","/","?",";",":"];
 
-var numbers = ["0","1","2","3","4","5","6","7","8","9"];
-var characterChoicesAll = capitalLetters,lowerCaseletters, specialCharacters, numbers;
-var passwordtextbox = document.getElementById("password");
-var passwordHolder = document.getElementById("passwordHolder");
-var password = "";
+    var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+    
+    var generateBtn = document.querySelector("#generate");
+    
+//writing new password
+var writeNewaPassword = function() {
+    var password = createNewPassword();
+    var passwordHolder = document.querySelector("#passwordHolder");
+    passwordHolder.value = password;
+
+    function randomize(){
+        for (var i= 0; i < length; i++) {
+            password += (Math.floor(Math.random() * characterChoicesAll.length));
+        }
+        return consol.log(password);
+    };
+
+}; 
+
 
 // functin to create password 
 var createNewPassword = function() {
+        var password ='';  
+        var characterChoicesAll = useOfNumbers, useOfcapitalLetters, useOflowerCaseLetters, useOfSpecialCharacters;
+        var choiceCreatePassword = alert("Get ready to get a new random computer generated password!");
+        //var of the prompts/confirm so user can pick what characteristics their password has
+        var length = prompt("Between 8 to 128 characters, how many characters would you like your password to have?");
+            if(length <8 || length >128 ) {  
+                prompt("Password length must be between 8 and 128 characters. How Many characters would you like your password to have?");   
+            } else {
+                console.log("User password will be " + length + " characters long.");  
+        };
 
-    var choiceCreatePassword = alert("Get ready to get a new random computer generated password!");
-    //var of the prompts/confirm so user can pick what characteristics their password has
-var numberOfcharacters = prompt("Between 8 to 128 characters, how many charachters would you like your password to have?");
-if(numberOfcharacters <8 || numberOfcharacters >128 ) {
-    prompt("Password length must be between 8 and 128 charachters. How Many characters would you like your password to have?");   
-} else {
-    console.log("User password will be " + numberOfcharacters + " characters long.");  
+    // confirm for use of capital letters; console log user choice.
+    var useOfcapitalLetters = confirm("Click OK to include capital letters.");
+        if (useOfcapitalLetters === true){
+            password += capitalLetters;
+            console.log("Capital letters will be included.");
+        } else {
+            console.log("Capital letters will NOT be included.");
+        };
+
+    //confirm for use of lower case letters; console log user choice.
+    var useOflowerCaseLetters = confirm("Click OK to include lower case letters.");
+        if (useOflowerCaseLetters === true){
+            password += lowerCaseletters;
+            console.log("Lowercase letters will be included.");
+        } else {
+            console.log("Lowercase letters will NOT be included.");
+        };
+
+    //confirm for use of special charachters; console log user choice.
+    var useOfSpecialCharacters = confirm("Click OK to use special charachters.");
+        if (useOfSpecialCharacters=== true){
+            password += specialCharacters;
+            console.log("Special characters will be included.");
+        } else {
+            console.log("Special characters will NOT be included.");
+        };
+
+    //confim for use of number; console log user choice.
+    var useOfNumbers = confirm("Click   OK to use numbers.");
+        if (useOfNumbers === true){
+            password += numbers;
+            console.log("Numbers will be included.");
+        } else {
+            console.log("Numbers will NOT be included.");
+        };
+
+    
 };
- var length = numberOfcharacters;
+ 
 
-// confirm for use of capital letters; console log user choice.
-var useOfcapitalLetters = confirm("Click OK to include capital letters.");
-if (useOfcapitalLetters === true){
-    password += capitalLetters;
-    console.log("Capital letters will be included.");
-} else {
-    console.log("Capital letters will NOT be included.");
-};
+    //console.log (passwordHolder.value = password(length.value, characterChoicesAll));
+    
 
-//confirm for use of lower case letters; console log user choice.
-var useOflowerCaseLetters = confirm("Click OK to include lower case letters.");
-if (useOflowerCaseLetters === true){
-    password += lowerCaseletters;
-    console.log("Lowercase letters will be included.");
-} else {
-    console.log("Lowercase letters will NOT be included.");
-};
-
-//confirm for use of special charachters; console log user choice.
-var useOfSpecialCharacters = confirm("Click OK to use special charachters.");
-if (useOfSpecialCharacters=== true){
-    password += specialCharacters;
-    console.log("Special characters will be included.");
-} else {
-    console.log("Special characters will NOT be included.");
-};
-
-//confim for use of number; console log user choice.
-var useOfNumbers = confirm("Click   OK to use numbers.");
-if (useOfNumbers === true){
-    password += numbers;
-    console.log("Numbers will be included.");
-} else {
-    console.log("Numbers will NOT be included.");
-};
-
-console.log (passwordHolder.value = password(length.value, characterChoicesAll));
+    // console.log(password(characterChoicesAll));
+    // console.log(password.join(''));
+    // return password.join('');
 
 
-
-
-};
-
-
+generateBtn.addEventListener("click", writeNewaPassword);
 // console.log(generatePass(12, charactherChoicesAll));
 
 // function generatePass(length, charactherChoicesAll) {
