7db193ac0bdd1770a7ab9b85d96193e7f680c05f
aydanegash <73369360+aydanegash@users.noreply.github.com>
2021-05-19 05:11:07 -0500

    worked on combining the selected elements to create a password

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
  //welcome message
-var password= "";
+//var password= "";
 //Array of different charachters that can be used in password
 var capitalLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
                         "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
@@ -8,29 +8,32 @@ var capitalLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
 var lowerCaseletters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n",
                         "o","p","q","r","s","t","u","v","w","x","y","z"];
 
-var specialCharachters = ["`","~","!","@","#","$","%","^","&","*","(",")","-","_",
+var specialCharacters = ["`","~","!","@","#","$","%","^","&","*","(",")","-","_",
                             "=","+",",",".","<",">","/","?",";",":"];
 
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
-var charactherChoicesAll = capitalLetters,lowerCaseletters, specialCharachters, numbers;
-
+var characterChoicesAll = capitalLetters,lowerCaseletters, specialCharacters, numbers;
+var passwordtextbox = document.getElementById("password");
+var passwordHolder = document.getElementById("passwordHolder");
+var password = "";
 
 // functin to create password 
 var createNewPassword = function() {
 
     var choiceCreatePassword = alert("Get ready to get a new random computer generated password!");
     //var of the prompts/confirm so user can pick what characteristics their password has
-var numberOfcharachters = prompt("Between 8 to 128 characters, how many charachters would you like your password to have?");
-if(numberOfcharachters <8 || numberOfcharachters >128 ) {
+var numberOfcharacters = prompt("Between 8 to 128 characters, how many charachters would you like your password to have?");
+if(numberOfcharacters <8 || numberOfcharacters >128 ) {
     prompt("Password length must be between 8 and 128 charachters. How Many characters would you like your password to have?");   
 } else {
-    console.log("User password will be " + numberOfcharachters + " characters long.");  
+    console.log("User password will be " + numberOfcharacters + " characters long.");  
 };
- var length = numberOfcharachters;
+ var length = numberOfcharacters;
 
 // confirm for use of capital letters; console log user choice.
 var useOfcapitalLetters = confirm("Click OK to include capital letters.");
 if (useOfcapitalLetters === true){
+    password += capitalLetters;
     console.log("Capital letters will be included.");
 } else {
     console.log("Capital letters will NOT be included.");
@@ -39,45 +42,49 @@ if (useOfcapitalLetters === true){
 //confirm for use of lower case letters; console log user choice.
 var useOflowerCaseLetters = confirm("Click OK to include lower case letters.");
 if (useOflowerCaseLetters === true){
+    password += lowerCaseletters;
     console.log("Lowercase letters will be included.");
 } else {
     console.log("Lowercase letters will NOT be included.");
 };
 
 //confirm for use of special charachters; console log user choice.
-var useOfSpecialCharachters = confirm("Click OK to use special charachters.");
-if (useOfSpecialCharachters=== true){
+var useOfSpecialCharacters = confirm("Click OK to use special charachters.");
+if (useOfSpecialCharacters=== true){
+    password += specialCharacters;
     console.log("Special characters will be included.");
 } else {
-    console.log("Special characthers will NOT be included.");
+    console.log("Special characters will NOT be included.");
 };
 
 //confim for use of number; console log user choice.
 var useOfNumbers = confirm("Click   OK to use numbers.");
 if (useOfNumbers === true){
+    password += numbers;
     console.log("Numbers will be included.");
 } else {
     console.log("Numbers will NOT be included.");
 };
 
+console.log (passwordHolder.value = password(length.value, characterChoicesAll));
 
-function generatePass() {
-    for (i=0;i<len;i++) {
-        password+=capitalLetters.charAt(Math.floor(Math.random()*capitalLetters.length));
-        password+=lowerCaseletters.charAt(Math.floor(Math.random()*lowerCaseletters.length));
-        password+=specialCharachters.charAt(Math.floor(Math.random()*specialCharachters.length));
-        password+=numbers.charAt(Math.floor(Math.random()*numbers.length));
-    };
 
-    password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
-    console.log(password);
-    return password;
-};
 
 
 };
 
 
+// console.log(generatePass(12, charactherChoicesAll));
+
+// function generatePass(length, charactherChoicesAll) {
+//     var password = "";
+//     for (var i = 0; i<1; i++){
+//         password += charactherChoicesAll.charAt(Math.floor(Math.random() * charactherChoicesAll.length));
+//     }
+//     return password;
+// }; 
+
+//https://www.youtube.com/watch?v=i6cX05cyIgc
 
 
 
