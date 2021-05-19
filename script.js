f61e0ce891151f5745fa88a9f4521fff28c192d9
aydanegash <73369360+aydanegash@users.noreply.github.com>
2021-05-19 17:49:12 -0500

    patched the excessive comma's found in generated password

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 
 //Array of different characters that can be used in password
-    var capitalLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
-                            "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    var capitalLetters = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
 
-    var lowerCaseletters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n",
-                            "o","p","q","r","s","t","u","v","w","x","y","z"];
+    var lowerCaseletters = ['abcdefghijklmnopqrstuvwxyz'];
 
-    var specialCharacters = ["`","~","!","@","#","$","%","^","&","*"];
+    var specialCharacters = ['~!@#$%^&*+='];
 
-    var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+    var numbers = ["1234567890"];
     
     var generateBtn = document.querySelector("#generate");
     
